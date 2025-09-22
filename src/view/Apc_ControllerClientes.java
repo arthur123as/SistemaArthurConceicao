fd4977b4ad6d464fc76ed83836a009b6bc166087
arthur123as <arthur.conceicao@estudante.ifms.edu.br>
2025-09-22 00:23:50 -0400

    realizei a clonagem das telas de cadastro e principal do projeto do último semestre e formatei desde o zero, ou seja, deixei os formulários sem os antigos metodos de beanView, habilitarDesabilitar, etc.

---
+++ src/view/Apc_ControllerClientes.java
@@ -0,0 +1,73 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package view;
+
+import bean.Apc_Clientes;
+import java.util.List;
+import javax.swing.table.AbstractTableModel;
+
+/**
+ *
+ * @author u09947038130
+ */
+public class Apc_ControllerClientes extends AbstractTableModel {
+    
+    List lista;
+    
+    public void setList(List lista) {
+        this.lista = lista;
+    }
+    
+    public Object getBean(int rowIndex) {
+        return lista.get(rowIndex);
+    }
+    
+    @Override
+    public int getRowCount() {
+        return lista.size();
+    }
+
+    @Override
+    public int getColumnCount() {
+        return 4;
+    }
+
+    @Override
+    public Object getValueAt(int rowIndex, int columnIndex) {
+        Apc_Clientes apc_clientes = (Apc_Clientes) lista.get(rowIndex);
+        if (columnIndex == 0) {
+            return apc_clientes.getApc_idClientes();
+        } 
+        
+        if (columnIndex == 1) {
+            return apc_clientes.getApc_nome();
+        } 
+        
+        if (columnIndex == 2) {
+            return apc_clientes.getApc_cpf();
+        }
+        
+        if (columnIndex == 3) {
+            return apc_clientes.getApc_dataCadastro();
+        }
+        
+        if (columnIndex == 4) {
+            return apc_clientes.getApc_ativo();
+        }
+        
+        return "";
+    }
+    
+    @Override
+    public String getColumnName (int column) {
+        if (column == 0) return "Código";
+        if (column == 1) return "Nome";
+        if (column == 2) return "Cpf";
+        if (column == 3) return "Data de Cadastro";
+        if (column == 4) return "Ativo";
+        
+        return "";
+    }
+}
---
+++ src/view/Apc_ControllerVendedor.java
@@ -0,0 +1,78 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package view;
+
+import bean.Apc_Vendedor;
+import java.util.List;
+import javax.swing.table.AbstractTableModel;
+
+/**
+ *
+ * @author u09947038130
+ */
+public class Apc_ControllerVendedor extends AbstractTableModel {
+    
+    List lista;
+    
+    public void setList(List lista) {
+        this.lista = lista;
+    }
+    
+    public Object getBean(int rowIndex) {
+        return lista.get(rowIndex);
+    }
+    
+    @Override
+    public int getRowCount() {
+        return lista.size();
+    }
+
+    @Override
+    public int getColumnCount() {
+        return 6;
+    }
+
+    @Override
+    public Object getValueAt(int rowIndex, int columnIndex) {
+        Apc_Vendedor apc_vendedor = (Apc_Vendedor) lista.get(rowIndex);
+        if (columnIndex == 0) {
+            return apc_vendedor.getApc_idVendedor();
+        } 
+        
+        if (columnIndex == 1) {
+            return apc_vendedor.getApc_nome();
+        } 
+        
+        
+        if (columnIndex == 2) {
+            return apc_vendedor.getApc_cpf();
+        }
+        
+        if (columnIndex == 3) {
+            return apc_vendedor.getApc_dataNascimento();
+        }
+        
+        if (columnIndex == 4) {
+            return apc_vendedor.getApc_cargo();
+        }
+        
+        if (columnIndex == 5) {
+            return apc_vendedor.getApc_dataAdimissao();
+        }
+        return "";
+    }
+    
+    @Override
+    public String getColumnName (int column) {
+        if (column == 0) return "Código";
+        if (column == 1) return "Nome";
+        if (column == 2) return "Cpf";
+        if (column == 3) return "Data Nascimento";
+        if (column == 4) return "Cargo";
+        if (column == 5) return "Data Adimissão";
+        
+        return "";
+    }
+}
